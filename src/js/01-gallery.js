133b84035fdc29b7492dd033362a44bef37f64f2
Otrzewik <trzewik.ola.aleksandra@gmail.com>
2023-12-11 21:22:05 +0100

    new

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function renderGallery() {
+  galleryItems.forEach(item => {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
+
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.alt = item.description;
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    galleryContainer.appendChild(galleryItem);
+  });
+}
+
+renderGallery();
+
+new SimpleLightbox('.gallery a', {
+  /* options */
+});
+
+console.log(galleryItems);
